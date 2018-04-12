0f115aab7bf508d73b5494a797879ddbd4b87270
James V. Soukup <jvs@apep.uci.edu>
2018-04-11 23:47:25 -0700

    Initialization of constants

--- Viscosity Temperature Dependence/problem1.py
+++ Viscosity Temperature Dependence/problem1.py
@@ -9,3 +9,17 @@
 # Import the usual suspects
 import numpy as np
 import matplotlib.pyplot as plt
+
+##############
+# Initialize #
+##############
+
+## Measured Values
+# Measured Temperature [degrees Celsius]
+measTemp = np.matrix([0.0, 5.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0])
+# Measured Viscosity (10^-3) [Pa*s]
+measVisc = np.matrix([1.79, 1.52, 1.31, 1.00, 0.80, 0.65, 0.55, 0.47, 0.40, 0.35, 0.31])
+
+## System Invariant
+# Ambient Temperature
+ambTemp = 20.0
