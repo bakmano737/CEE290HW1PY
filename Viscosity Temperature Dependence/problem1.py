4b6d74d141d511a89ecddc68d72f328f98f05d14
James V. Soukup <jvs@apep.uci.edu>
2018-04-11 23:46:30 -0700

    Add problem 1 file with numpy/pyplot header

---
+++ Viscosity Temperature Dependence/problem1.py
@@ -0,0 +1,11 @@
+############################################################
+# ----------- University of California, Irvine ----------- #
+# ------------- CEE 290 Mergin Models & Data ------------- #
+# --------------- Homework #1 - Problem #1 --------------- #
+# ---- Temperature Dependence of Viscosity of a Fluid ---- #
+# --------------- James V. Soukup 85841994 --------------- #
+############################################################
+
+# Import the usual suspects
+import numpy as np
+import matplotlib.pyplot as plt
